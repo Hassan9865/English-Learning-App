d72960f6c8b6697243928488d2908f941bc12eb7
Hassan <syedhassanraza0311@gmail.com>
2024-11-01 12:14:15 +0500

    edit quiz

--- lib/view/Quiz/basic_quiz/basic_quiz_viewModel.dart
+++ lib/view/Quiz/basic_quiz/basic_quiz_viewModel.dart
@@ -4,42 +4,35 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class BasicQuizViewModel extends BaseViewModel {
-  int currentQuestionIndex = 0; // Track the current question index
-  int? selectedOptionIndex; // Track the selected option
-  bool answered = false; // Check if the question has been answered
+  int currentQuestionIndex = 0;
+  int? selectedOptionIndex;
+  bool answered = false;
 
-  // Method to select an option
   void selectOption(int index, int correctAnswerIndex) {
-    selectedOptionIndex = index; // Save the selected option
-    answered = true; // Mark the question as answered
-    notifyListeners(); // Notify UI to update
+    selectedOptionIndex = index;
+    answered = true;
+    notifyListeners();
   }
 
-  // Method to check if the selected option is correct
   bool isCorrect(int optionIndex, int correctAnswerIndex) {
     return optionIndex == correctAnswerIndex;
   }
 
-  // Method to reset for the next question
   void reset() {
-    selectedOptionIndex = null; // Reset selected option
-    answered = false; // Mark the question as not answered
-    notifyListeners(); // Notify UI to update
+    selectedOptionIndex = null;
+    answered = false;
+    notifyListeners();
   }
 
-  // Method to move to the next question
   void nextQuestion(BuildContext context) {
     if (currentQuestionIndex < basicQuiz.length - 1) {
-      currentQuestionIndex++; // Increment the question index
+      currentQuestionIndex++;
     } else {
-      // Handle the case when the quiz is completed
-      // You can navigate to a results page or show a message
-
       showAlertDialog(
         context,
       );
     }
-    reset(); // Reset for the new question
-    notifyListeners(); // Notify UI to update
+    reset();
+    notifyListeners();
   }
 }
